7dc9a29d213626f0b6c3e2e6447550d12a4aecb8
jeremyfelt <jeremy.felt@gmail.com>
2015-03-15 10:51:33 -0700

    Add event venue to the JSON response for events

--- wsuwp-extended-events-calendar.php
+++ wsuwp-extended-events-calendar.php
@@ -29,6 +29,7 @@ class WSU_Extended_Events_Calendar {
 			return $_post;
 		}
 
+		$_post['meta']['event_venue'] = tribe_get_venue( $post['ID'] );
 		$_post['meta']['start_date'] = get_post_meta( $post['ID'], '_EventStartDate', true );
 		$_post['meta']['end_date'] = get_post_meta( $post['ID'], '_EventEndDate', true );
 
