b5a3b80b633353a8db6a80da9564656150d49b83
jeremyfelt <jeremy.felt@gmail.com>
2015-03-15 10:09:01 -0700

    Filter WP API response data to include start and end date

--- wsuwp-extended-events-calendar.php
+++ wsuwp-extended-events-calendar.php
@@ -13,7 +13,26 @@ class WSU_Extended_Events_Calendar {
 	 * Setup hooks.
 	 */
 	public function __construct() {
+		add_filter( 'json_prepare_post', array( $this, 'prepare_calendar_event' ), 10, 2 );
 	}
 
+	/**
+	 * Add specific meta information to a calendar item.
+	 *
+	 * @param array  $_post   Data eventually passed for the post to the API response.
+	 * @param array  $post    Current post data.
+	 *
+	 * @return mixed
+	 */
+	public function prepare_calendar_event( $_post, $post ) {
+		if ( ! isset( $post['post_type'] ) || 'tribe_events' !== $post['post_type'] ) {
+			return $_post;
+		}
+
+		$_post['meta']['start_date'] = get_post_meta( $post['ID'], '_EventStartDate', true );
+		$_post['meta']['end_date'] = get_post_meta( $post['ID'], '_EventEndDate', true );
+
+		return $_post;
+	}
 }
 new WSU_Extended_Events_Calendar();
