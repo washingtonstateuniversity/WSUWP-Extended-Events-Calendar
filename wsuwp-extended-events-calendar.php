910881ea128d43a48c86bbe2e27484213cea9a4c
jeremyfelt <jeremy.felt@gmail.com>
2015-12-08 15:05:42 -0800

    Register the custom fields used to pass event information via REST

--- wsuwp-extended-events-calendar.php
+++ wsuwp-extended-events-calendar.php
@@ -14,6 +14,7 @@ class WSU_Extended_Events_Calendar {
 	 */
 	public function __construct() {
 		add_filter( 'json_prepare_post', array( $this, 'prepare_calendar_event' ), 10, 2 );
+		add_action( 'rest_api_init', array( $this, 'register_api_fields' ) );
 		add_filter( 'tribe_events_pro_recurrence_batch_size', array( $this, 'limit_recurring_batch_size' ), 10 );
 		add_filter( 'tribe_events_register_event_type_args', array( $this, 'register_events_endpoint' ) );
 	}
@@ -54,6 +55,8 @@ class WSU_Extended_Events_Calendar {
 	/**
 	 * Update the `register_post_type()` arguments for The Events Calendar to support an /events/ endpoint.
 	 *
+	 * @since 0.1.0
+	 *
 	 * @param array $args
 	 *
 	 * @return array
@@ -63,6 +66,57 @@ class WSU_Extended_Events_Calendar {
 		$args['rest_base'] = 'events';
 
 		return $args;
+	}
+
+	/**
+	 * Retrieve the data to use when returning an event through the REST API.
+	 *
+	 * @param $object
+	 * @param $field
+	 * @param $request
+	 *
+	 * @return string
+	 */
+	public function get_api_meta_data( $object, $field, $request ) {
+		if ( 'event_city' === $field ) {
+			return esc_html( tribe_get_city( $object['id'] ) );
+		}
+
+		if ( 'event_state' === $field ) {
+			return esc_html( tribe_get_state( $object['id'] ) );
+		}
+
+		if ( 'event_venue' === $field ) {
+			return esc_html( tribe_get_venue( $object['id'] ) );
+		}
+
+		if ( 'start_date' === $field ) {
+			return esc_html( get_post_meta( $object['id'], '_EventStartDate', true ) );
+		}
+
+		if ( 'end_date' === $field ) {
+			return esc_html( get_post_meta( $object['id'], '_EventEndDate', true ) );
+		}
+
+		return '';
+	}
+
+	/**
+	 * Register the custom meta fields attached to a REST API response containing event data.
+	 *
+	 * @since 0.1.0
+	 */
+	public function register_api_fields() {
+		$args = array(
+			'get_callback' => array( $this, 'get_api_meta_data' ),
+			'update_callback' => 'esc_html',
+			'schema' => null,
+		);
+		register_api_field( 'tribe_events', 'event_city', $args );
+		register_api_field( 'tribe_events', 'event_state', $args );
+		register_api_field( 'tribe_events', 'event_venue', $args );
+		register_api_field( 'tribe_events', 'start_date', $args );
+		register_api_field( 'tribe_events', 'end_date', $args );
 	}
 }
 new WSU_Extended_Events_Calendar();
