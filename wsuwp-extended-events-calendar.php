a6957b10f1dcffb6252d8d388828f171f472a76c
jeremyfelt <jeremy.felt@gmail.com>
2015-12-08 15:06:11 -0800

    Remove the old filter used to prepare the JSON REST response

--- wsuwp-extended-events-calendar.php
+++ wsuwp-extended-events-calendar.php
@@ -13,32 +13,9 @@ class WSU_Extended_Events_Calendar {
 	 * Setup hooks.
 	 */
 	public function __construct() {
-		add_filter( 'json_prepare_post', array( $this, 'prepare_calendar_event' ), 10, 2 );
 		add_action( 'rest_api_init', array( $this, 'register_api_fields' ) );
 		add_filter( 'tribe_events_pro_recurrence_batch_size', array( $this, 'limit_recurring_batch_size' ), 10 );
 		add_filter( 'tribe_events_register_event_type_args', array( $this, 'register_events_endpoint' ) );
-	}
-
-	/**
-	 * Add specific meta information to a calendar item.
-	 *
-	 * @param array  $_post   Data eventually passed for the post to the API response.
-	 * @param array  $post    Current post data.
-	 *
-	 * @return mixed
-	 */
-	public function prepare_calendar_event( $_post, $post ) {
-		if ( ! isset( $post['post_type'] ) || 'tribe_events' !== $post['post_type'] ) {
-			return $_post;
-		}
-
-		$_post['meta']['event_city'] = esc_html( tribe_get_city( $post['ID'] ) );
-		$_post['meta']['event_state'] = esc_html( tribe_get_state( $post['ID'] ) );
-		$_post['meta']['event_venue'] = esc_html( tribe_get_venue( $post['ID'] ) );
-		$_post['meta']['start_date'] = esc_html( get_post_meta( $post['ID'], '_EventStartDate', true ) );
-		$_post['meta']['end_date'] = esc_html( get_post_meta( $post['ID'], '_EventEndDate', true ) );
-
-		return $_post;
 	}
 
 	/**
